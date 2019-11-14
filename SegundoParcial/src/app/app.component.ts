92f9d0685dcafa585ae45853021173ec5356b2bb
federicotomadintangol <federico@tangol.com>
2019-11-14 10:46:20 -0300

    inicio del proyecto

---
+++ SegundoParcial/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'SegundoParcial';
+
+constructor() {
+
+  firebase.s
+
+
+}
+
+
+}
---
+++ SegundoParcial/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyCS45HV9b7blcGjbbUsD-J3ec1kz-H8x5E",
+    authDomain: "segundoparcial-129dc.firebaseapp.com",
+    databaseURL: "https://segundoparcial-129dc.firebaseio.com",
+    projectId: "segundoparcial-129dc",
+    storageBucket: "segundoparcial-129dc.appspot.com",
+    messagingSenderId: "756484251410",
+    appId: "1:756484251410:web:24948ca8d665ea91b3ddcb",
+    measurementId: "G-38RHNDYT3M"
+}
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
