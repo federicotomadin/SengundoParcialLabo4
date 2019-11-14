ef1161fa54472d65829d55bae01ea1c571ce88a6
federicotomadintangol <federico@tangol.com>
2019-11-14 11:22:40 -0300

    inicio de proyecto

--- SegundoParcial/src/app/app.module.ts
+++ SegundoParcial/src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { AdministradorComponent } from './componentes/administrador/administrador.component';
+import { AlumnoComponent } from './componentes/alumno/alumno.component';
+import { ProfesorComponent } from './componentes/profesor/profesor.component';
+import { MateriaComponent } from './componentes/materia/materia.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    AdministradorComponent,
+    AlumnoComponent,
+    ProfesorComponent,
+    MateriaComponent
   ],
   imports: [
     BrowserModule,
